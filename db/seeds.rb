7599bccdf31d092d3b554d6a5ca78d6165c59ce8
Lui Kohl <lui.kohl@gmail.com>
2018-05-22 07:55:55 +0200

    changed booking model to work with review, added basic seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+u = User.create(email: "blake@yahoo.co.uk", password: "password")
+h = Host.create()
+b = Booking.create(user_id: u.id, host_id: h.id)
