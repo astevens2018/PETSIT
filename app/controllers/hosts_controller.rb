af9c15a255fd8b7ffd529db3514f38fa5f363665
Uma Patel <umabpatel@gmail.com>
2018-05-22 11:17:04 +0200

    controller for host template

---
+++ app/controllers/hosts_controller.rb
@@ -0,0 +1,30 @@
+class HostsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+    private
+
+    def host_params
+      params.require(:host).permit(:name, :email, :phone_number, :description, :address, :price, :pet_type)
+    end
+
+end
