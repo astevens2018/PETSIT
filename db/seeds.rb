1807b6d35d316b09b29897f90c96458f589fe6ae
Uma Patel <umabpatel@gmail.com>
2018-05-22 12:33:46 +0200

    some more

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-u = User.create(email: "blake@yahoo.co.uk", password: "password")
-h = Host.create()
+
+h = Host.create(name: "John", email: "john@gmail.com", phone_number: "+4406456", description: "Love walking dogs", address: "144 Kollwitz Str.", price: 45, rating: 4, pet_type: "Dog")
 b = Booking.create(user_id: u.id, host_id: h.id)
+u = User.new
