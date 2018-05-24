51fe5d9e45fbec32f8c0bca5b8a56e30bb9c000d
Lui Kohl <lui.kohl@gmail.com>
2018-05-24 17:18:03 +0200

    dash is done

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,6 @@
 class ProfilesController < ApplicationController
 
   def show
-
     @user_bookings = Booking.where(user_id: params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root to: 'pages#home'
 
   resources :hosts do
     resources :bookings
   end
-  get '/profile/:id', to: 'profiles#show'
+  get '/profile/:id', to: 'profiles#show', as: 'profile'
 end
