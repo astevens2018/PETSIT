21c2ce358c2be48339c26b4c59707d19f98e5b97
Uma Patel <umabpatel@gmail.com>
2018-05-24 13:06:06 +0200

    form change

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,6 @@ class Booking < ApplicationRecord
   belongs_to :user
   has_many :reviews
   validates :pet_name, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
