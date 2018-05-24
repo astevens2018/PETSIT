b49a908a226ca6c4a9438b47e2f1b8fd8b37267f
Alex Stevens <astev2018@gmail.com>
2018-05-24 10:52:05 +0200

    wip

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,39 +2,44 @@ Host.destroy_all
 h1 = Host.create(
   name: "John",
   email: "john@gmail.com",
-  phone_number: "+4406456",
+  phone_number: "+44789706456",
   description: "Love walking dogs",
   address: "144 Kollwitz Str.",
   price: 45,
   rating: 4,
+  username: "fabpot",
   pet_type: "Dog")
+
 
 h2 = Host.create(
   name: "Jack",
   email: "jack@gmail.com",
-  phone_number: "+645600",
+  phone_number: "+6457897600",
   description: "Love cats and dogs",
   address: "167 Olah Str.",
   price: 75,
   rating: 3,
+  username: "andrew",
   pet_type: "Cat")
 
 h3 = Host.create(
   name: "Alice",
   email: "alice@gmail.com",
-  phone_number: "+45555556",
+  phone_number: "+4555789556",
   description: "Big fan of turtles",
   address: "76 Black Str.",
   price: 87,
   rating: 5,
+  username: "egoist",
   pet_type: "Turtle")
 
 h4 = Host.create(
   name: "Dominik",
   email: "dominik@gmail.com",
-  phone_number: "+4924555556",
+  phone_number: "+4924588755556",
   description: "Fond of sheeps",
   address: "67 Herman Str.",
   price: 25,
   rating: 1,
+  username: "taylorotwell",
   pet_type: "Goat")
