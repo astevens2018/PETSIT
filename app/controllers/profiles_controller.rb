330058e056fbcf1e60c1216d3169ff9f74f09df6
Lui Kohl <lui.kohl@gmail.com>
2018-05-25 12:32:39 +0200

    saving map fix, first attempt at delete

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,4 +9,13 @@ class ProfilesController < ApplicationController
       lng: @host.longitude
     }]
   end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    # redirecting to the index
+    redirect_to profile_path(current_user)
+  end
+
+  end
 end
